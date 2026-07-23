5ff54587f35ae7509514a6f003c6f4c68874b6eb
muhmdshamil <shamiilmuhmd@gmail.com>
2026-07-23 14:46:41 +0530

    image to webp

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ["react-icons"],
   },
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
